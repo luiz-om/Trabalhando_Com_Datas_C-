d4d4b8420e7f127575c855b32c9b9b99276fa9a6
LuizOM <luizfernando_martins@hotmail.com>
2023-07-06 13:40:30 -0400

    CONSULTA DATAS

--- Program.cs
+++ Program.cs
@@ -5,3 +5,14 @@ Console.WriteLine("Hello, World!");
 var data = DateTime.Now;
 
 System.Console.WriteLine(data );
+//FORMATAÇÃO DE DATAS
+//somente o ano
+//var dataFormatada = String.Format("{0:yyyy}", data);
+//
+var dataFormatada = String.Format("{0:yy}", data);
+
+System.Console.WriteLine( dataFormatada );
+
+// PARA MAIORES INFROMACOES CONSUTLAR 
+
+// https://learn.microsoft.com/pt-br/dotnet/standard/base-types/custom-date-and-time-format-strings
